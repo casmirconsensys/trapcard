d043a70952893bb500eed7202deb53af4aefc014
alt-franz <examdeskseries@gmail.com>
2024-01-15 00:11:41 +0000

    password recovery intial setup

---
+++ src/lib/actions/updatePassword.ts
@@ -0,0 +1,36 @@
+'use server';
+
+import { createClient } from '@/utils/supabase/server';
+import { AuthError } from '@supabase/supabase-js';
+import { cookies } from 'next/headers';
+import { redirect } from 'next/navigation';
+
+export async function updatePassword(formData: FormData) {
+    const email = formData.get('email') as string;
+    const password = formData.get('password') as string;
+    const cookieStore = cookies();
+    const supabase = createClient(cookieStore);
+
+    const { data, error } = await supabase.auth.updateUser({
+        email,
+        password,
+        // data: {
+        //     successMsg: 'Password updated successfully',
+        //     errorMsg: 'Sorry please try again!!'
+        // }
+    })
+
+    if (error) {
+        console.error(error);
+        if (error instanceof AuthError) {
+            return redirect(`/account/password/update/?msg=${error.message}`);
+        }
+        return redirect(
+            '/account/password/update/msg=password-update failed. Please check your credentials and try again.'
+        );
+    }
+
+    if (data.user && data.user.id) {
+        return redirect('/account/login');
+    }
+}
