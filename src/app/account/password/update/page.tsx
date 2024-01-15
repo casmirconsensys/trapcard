893d1869cb146d738b6ea6438044f92287398ad0
alt-franz <examdeskseries@gmail.com>
2024-01-15 09:24:43 +0000

    remove email option for updating password

--- src/app/account/password/update/page.tsx
+++ src/app/account/password/update/page.tsx
@@ -8,7 +8,7 @@ const Update = () => {
     <HomeContainer>
       <div className='flex justify-center items-center'>
         <form action={updatePassword} className='md:w-96 space-y-6 bg-white/50 rounded-md shadow-xl p-4 backdrop-blur-sm'>
-          <input type="email" name='email' placeholder='example@gmail.com' className='w-full text-sm py-2 px-4 outline-none border rounded-md' />
+          {/* <input type="email" name='email' placeholder='example@gmail.com' className='w-full text-sm py-2 px-4 outline-none border rounded-md' /> */}
           <input type="password" name='password' placeholder='password' className='w-full text-sm py-2 px-4 outline-none border rounded-md' />
           <UpdateBtn />
         </form>
