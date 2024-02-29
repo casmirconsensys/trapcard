02814abfd336a258f1523391fc297e085c8405f9
@askcasmir <casmirconsults@gmail.com>
2024-02-29 18:01:47 -0500

    ERC6551:Registers & License an Video-NFT as an IP Remix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,33 @@ import Link from 'next/link';
 import { Montserrat } from 'next/font/google';
 import localFont from 'next/font/local';
 import type { Metadata } from 'next';
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import './globals.css';
+import { Web3Provider } from '../utils/Web3Provider';
+
+//storyprotocol
+const inter = Inter({ subsets: ['latin'] });
+
+export const metadata: Metadata = {
+  title: 'Story Protocol React SDK Test',
+  description: 'Story Protocol React SDK Test',
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <Web3Provider>
+        <body className={inter.className}>{children}</body>
+      </Web3Provider>
+    </html>
+  );
+}
+//storyprotocol 
 
 const mont = Montserrat({
   subsets: ['latin'],
